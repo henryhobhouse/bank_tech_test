88db493620e60eda30645e9c51cabbf4ebb90ef5
Henry Hobhouse <henryhobhouse@gmail.com>
2017-06-12 12:29:03 +0100

    Bank can create new account

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,14 @@
+require './app/lib/account.rb'
 # App controller for Bank
 class Bank
   attr_reader :accounts
 
   def initialize
     @accounts = []
+    @available_acc_number = 11111111
+  end
+
+  def new_account
+    @accounts.push(Account.new)
   end
 end
--- spec/unit/bank_spec.rb
+++ spec/unit/bank_spec.rb
@@ -5,4 +5,9 @@ describe Bank do
   it 'bank has no accounts on initialize' do
     expect(subject.accounts).to be_empty
   end
+
+  it 'accounts can have a new account' do
+    subject.new_account
+    expect(subject.accounts.count).to eq 1
+  end
 end
