fc96fc0daff87b3de224418d171c118e98311a84
Henry Hobhouse <henryhobhouse@gmail.com>
2017-06-12 11:50:07 +0100

    first test written and passed

---
+++ app/app.rb
@@ -0,0 +1,9 @@
+#App controller for Bank
+class Bank
+  attr_reader :accounts
+
+  def initialize()
+    @accounts = []
+  end
+
+end
---
+++ spec/unit/bank_spec.rb
@@ -0,0 +1,9 @@
+# Bank exists
+describe Bank do
+  let(:bank) { Bank.new }
+
+  it 'bank has no accounts on initialize' do
+    expect(subject.accounts).to be_empty
+  end
+
+end
